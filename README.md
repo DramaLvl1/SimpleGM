4bef7672709ca49484d5d5c57a912f2d2c0c3d2d
DramaLvl1 <53279807+DramaLvl1@users.noreply.github.com>
2020-08-02 13:11:28 +0200

    Add files via upload

---
+++ README.md
@@ -0,0 +1,19 @@
+# SimpleGM
+Hi this is my first Plugin i upload on Github. Its a SimpleGM plugin for Pocketmine.
+For Updates just wait for it later i will add a Config for it :D
+[![Download Count](https://poggit.pmmp.io/shield.dl.total/SimpleGM)](https://poggit.pmmp.io/p/SimpleGM)
+
+# About this Plugin
+You can type /gm <0/1/2/3> (You dont have to type a Player name you can. just like /gamemode)
+This Plugin was made by Drama_Lvl1
+Permission: simplegm.use
+
+# About me
+My Youtube Channel: [Drama_Lvl1](https://www.youtube.com/channel/UCbTymCVkV54kARmf95LXy_A?view_as=subscriber)
+My Coding Livestream: [Livestream](https://www.youtube.com/watch?v=2CSYLQqkF18&t=995s)
+My Main language: German
+Discord: Drama_Lvl1 YT#0282
+[Discord Server](https://discord.gg/THDKFGQ)
+
+# Whats Special About that?
+Its just like normal /gamemode but you just have to write /gm!
---
+++ src/SimpleGM/Drama_Lvl1/Main.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace SimpleGM\Drama_Lvl1;
+
+use pocketmine\command\Command;
+use pocketmine\command\CommandSender;
+use pocketmine\plugin\PluginBase;
+use pocketmine\Player;
+
+class Main extends PluginBase{
+
+    public function onCommand(CommandSender $sender, Command $cmd, string $label, array $args) : bool {
+        if($cmd->getName() === "gm"){
+            if(isset($args[0])){
+                if($sender->hasPermission("simplegm.use")){
+                    if(isset($args[1])){
+                        $player = $this->getServer()->getPlayer($args[1]);
+                        if($player == null) {
+                            $sender->sendMessage("§8[§bSimple§3GM§8] §cCant found this Player!");
+                            return true;
+                        }
+                        $change = $player->getName();
+						
+                        if($args[0] === "0"){
+                            $player->setGamemode(0);
+                            $player->sendMessage("§8[§bSimple§3GM§8] §aYour Gamemode has been changed to §2survival");
+                            $sender->sendMessage("§8[§bSimple§3GM§8] §aYou changed the Gamemode from §2" . $change . "§a to §2survival");
+                        } else if($args[0] === "1"){
+                            $player->setGamemode(1);
+                            $player->sendMessage("§8[§bSimple§3GM§8] §aYour Gamemode has been changed to §2creative");
+                            $sender->sendMessage("§8[§bSimple§3GM§8] §aYou changed the Gamemode from §2" . $change . "§a to §2creative");
+                        } else if($args[0] === "2"){
+                            $player->setGamemode(2);
+                            $player->sendMessage("§8[§bSimple§3GM§8] aYour Gamemode has been changed to §2adventure");
+                            $sender->sendMessage("§8[§bSimple§3GM§8] §aYou changed the Gamemode from §2" . $change . "§a to §2adventure");
+                        } else if($args[0] === "3"){
+                            $player->setGamemode(3);
+                            $player->sendMessage("§8[§bSimple§3GM§8] §aYour Gamemode has been changed to §2spectator");
+                            $sender->sendMessage("§8[§bSimple§3GM§8] §aYou changed the Gamemode from §2" . $change . "§a to §2spectator");
+						}
+					} else {
+                        if($args[0] === "0"){
+                            $sender->setGamemode(0);
+                            $sender->sendMessage("§8[§bSimple§3GM§8] §aYou changed your Gamemode to §2survival");                        
+                        } else if($args[0] === "1"){
+                            $sender->setGamemode(1);
+                            $sender->sendMessage("§8[§bSimple§3GM§8] §aYou changed your Gamemode to §2creative");
+                        } else if($args[0] === "2"){
+                            $sender->setGamemode(2);
+                            $sender->sendMessage("§8[§bSimple§3GM§8] §aYou changed your Gamemode to §2adventure");
+                        } else if($args[0] === "3"){
+                            $sender->setGamemode(3);
+                            $sender->sendMessage("§8[§bSimple§3GM§8] §aYou changed your Gamemode to §2spectator");
+						}
+                    }
+				} else if(!$sender->hasPermission("SimpleGM.use")){
+					$sender->sendMessage("§8[§bSimple§3GM§8] §cDu hast keine Berechtigung diesen Befehl zu verwenden!");
+				}
+					
+            } else {
+                $sender->sendMessage("§8[§bSimple§3GM§8] §cUsage: §e/gm <0/1/2/3> <spieler> (it works without Playername too just like normal /gamemode)");
+            }
+        }
+    return true;
+    }
+}
